0717ea4cb4c93bc4de1b439b6863a7274a1d6cb9
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-04-09 00:54:32 -0500

    lat and lot data converted to function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,75 @@
-// const apiKey = '0e65efea80d9a0647c2d571b6099538a';
+const apiKey = "0e65efea80d9a0647c2d571b6099538a";
 
-// fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`)
-//   .then(function(response){
-//     console.log(response)
-//   })
+const searchedCities = JSON.parse(localStorage.getItem("searchedCities")) || [];
+
+function searchCity() {
+  const cityName = document.getElementById("cityInput").value;
+
+  // Check if input is not empty and not already searched
+  if (cityName && !searchedCities.includes(cityName)) {
+    searchedCities.unshift(cityName); // Add the city to the beginning of the array
+    savedCities();
+    displayCities();
+    document.getElementById("cityInput").value = ""; // Clear the input field
+  }
+
+  getCityData(cityName)
+}
+
+console.log(searchedCities);
 
 
-let searchedCities = JSON.parse(localStorage.getItem('searchedCities')) || [];
+function displayCities() {
+  const searchedCitiesElement = document.getElementById("searchedCities");
+  searchedCitiesElement.innerHTML = ""; // Clear previous cities
 
-    function searchCity() {
-        const input = document.getElementById('cityInput').value.trim();
+  // Display all searched cities
+  searchedCities.forEach((city) => {
+    const listItem = document.createElement("a");
+    listItem.href = "#"; // You can add a link to each city if you want
+    listItem.className =
+      "list-group-item list-group-item-action list-group-item-warning";
+    listItem.textContent = city;
+    searchedCitiesElement.appendChild(listItem);
+  });
+}
 
-        // Check if input is not empty and not already searched
-        if (input && !searchedCities.includes(input)) {
-            searchedCities.unshift(input); // Add the city to the beginning of the array
-            savedCities();
-            displayCities();
-            document.getElementById('cityInput').value = ''; // Clear the input field
-        }
+function savedCities() {
+  localStorage.setItem("searchedCities", JSON.stringify(searchedCities));
+}
+
+const button =  document.querySelector(".btn");
+button.addEventListener('click', searchCity )
+
+
+// 1.st request
+function getCityData(city){
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${searchedCities}&appid=${apiKey}&units=imperial`
+  ).then(function (response) {
+    if( !response.ok){
+      showError(`Please enter a valid city name.`)
     }
+    return response.json()
+  }).then(function(json){
+    console.log(json)
+    getForecast(json.coord.lat,json.coord.lon)
+  }) 
+   
+  
+}
 
-    function displayCities() {
-        const searchedCitiesElement = document.getElementById('searchedCities');
-        searchedCitiesElement.innerHTML = ''; // Clear previous cities
 
-        // Display all searched cities
-        searchedCities.forEach(city => {
-            const listItem = document.createElement('a');
-            listItem.href = '#'; // You can add a link to each city if you want
-            listItem.className = 'list-group-item list-group-item-action list-group-item-warning';
-            listItem.textContent = city;
-            searchedCitiesElement.appendChild(listItem);
-        });
-    }
+function getForecast(lat, lon){
+  
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
+  ).then(function (response) {
+    return response.json()
+  }).then(function(json){
+    
+  })
+  
+}
 
-    function savedCities() {
-        localStorage.setItem('searchedCities', JSON.stringify(searchedCities));
-    }
